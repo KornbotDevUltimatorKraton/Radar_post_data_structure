9c54b6aafd32723cc31a5b7b1dcebc9dc16fdf85
Chanapai Chuadchum <kornbot359@hotmail.com>
2025-03-02 22:29:38 +0700

    Add files via upload

--- radar_transmitter.py
+++ radar_transmitter.py
@@ -1,4 +1,4 @@
 import requests 
-reqdat = requests.post("http://192.168.50.193:5889/outdoor_navigation",json={'email':'kornbot380@hotmail.com','project_name':'Smart_Robots','payloaddat':
-{'radar':{'radar_1':{'X':160,'Y':520,'Z':20,'Xo':0,'Yo':0,'Zo':0,'roll':180,'pitch':90,'yaw':180}}}})
+reqdat = requests.post("http://192.168.50.193:5889/outdoor_navigation",json={'email':'kornbot380@hotmail.com','project_name':'Smart_Robots','payloaddat':{
+'radar':{'radar_1':{'X':170,'Y':672,'Z':340,'Xo':11,'Yo':10,'Zo':0,'Xc':0,'Yc':0,'Zc':0,'roll':180,'pitch':90,'yaw':180}}}})
 
